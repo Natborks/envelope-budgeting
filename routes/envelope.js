ea5b62e074c871cd8c0dd92ce0aee7a319b150ae
natborks <natborksdeutsch@yahoo.com>
2021-08-26 20:47:53 +0000

    create project and add express router

---
+++ routes/envelope.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = require('../app')
+const router = express()
+
+
+router.get('/', (req, res, next) => {
+    res.send('hello from the other side')
+})
+
+router.post('/', (req, res, next) => {
+    res.send('hello from post')
+})
+
+router.put('/envelopeId', (req, res, next) => {
+    res.send('hello from put')
+})
+
+router.delete('/:envelopeId', (req, res, next) =>{
+    res.send('hello from delete')
+})
+
+module.exports = router
