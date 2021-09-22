111612671def87b8d95f82d54e82fdf4671c7ec4
natborks <natborksdeutsch@yahoo.com>
2021-09-22 22:41:43 +0000

    use cors

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-var express = require('express');
-var requestInfologger = require('morgan');
-var logger = require('./util/logger')
-var middlerware = require('./util/middleware')
-var mongoose = require('mongoose')
-var config = require('./util/config')
+const express = require('express')
+const requestInfologger = require('morgan')
+const logger = require('./util/logger')
+const middlerware = require('./util/middleware')
+const mongoose = require('mongoose')
+const config = require('./util/config')
+const cors = require('cors')
 
-var envelopeRouter = require('./controllers/envelope')
+const envelopeRouter = require('./controllers/envelope')
 
-var app = express();
+const app = express()
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to db @:', config.MONGODB_URI);
+    logger.info('connected to db @:', config.MONGODB_URI)
   })
   .catch(error => {
     logger.error(error.message)
   })
 
-app.use(requestInfologger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
+app.use(requestInfologger('dev'))
+app.use(express.json())
+app.use(cors)
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/envelopes', envelopeRouter)
 
 // catch 404 and forward to error handler
-app.use(middlerware.unknownRoute);
+app.use(middlerware.unknownRoute)
 
 // error handler
-app.use(middlerware.errorHandler);
+app.use(middlerware.errorHandler)
 
 app.listen(config.PORT, () => {
-    logger.info(`listening on port ${config.PORT}`)
+  logger.info(`listening on port ${config.PORT}`)
 })
-module.exports = app;
+module.exports = app
